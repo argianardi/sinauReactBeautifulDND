ccadd008daf831276074bc53e8b60f8e7e950412
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-08-01 04:21:20 +0700

    Style: Refactor style use talwind css

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,42 +1,15 @@
 import React from 'react';
 import { Droppable } from 'react-beautiful-dnd';
-import { styled } from 'styled-components';
 import Task from './Task';
-
-const Container = styled.div`
-  background-color: #f4f5f7;
-  border-radius: 2.5px;
-  width: 300px;
-  height: 475px;
-  overflow-y: scroll;
-  -ms-overflow-style: none;
-  border: 1px solid gray;
-`;
-
-const Title = styled.h3`
-  position: sticky;
-  top: 0px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  background-color: pink;
-  text-align: center;
-`;
-
-const TaskList = styled.div`
-  padding: 3px;
-  transition: background-color 0.2s ease;
-  background-color: #f4f5f7;
-  flex-grow: 1;
-  min-height: 100px;
-`;
 
 const Column = ({ title, tasks, id }) => {
   return (
-    <Container className="column">
-      <Title style={{ backgroundColor: 'lightblue' }}>{title}</Title>
+    <div className="column w-80 h-[475px] bg-[#f4f5f7] rounded-sm border-[1px] border-gray-400">
+      <h3 className="sticky top-0 p-2 text-center bg-[#add8e6]">{title}</h3>
       <Droppable droppableId={id}>
         {(provided, snapshot) => (
-          <TaskList
+          <div
+            className="p-1 min-h-[100px]"
             ref={provided.innerRef}
             {...provided.droppableProps}
             isDraggingOver={snapshot.isDraggingOver}
@@ -45,10 +18,10 @@ const Column = ({ title, tasks, id }) => {
               <Task key={index} index={index} task={task} />
             ))}
             {provided.placeholder}
-          </TaskList>
+          </div>
         )}
       </Droppable>
-    </Container>
+    </div>
   );
 };
 
